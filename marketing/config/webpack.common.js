785628b40f5e955832bb477351ea021a8a7808ea
kusham <kusham1998@gmail.com>
2023-02-26 15:31:54 +0530

    error fix

--- marketing/config/webpack.common.js
+++ marketing/config/webpack.common.js
@@ -8,7 +8,7 @@ module.exports = {
           loader: "babel-loader",
           options: {
             presets: ["@babel/preset-react", "@babel/preset-env"],
-            plugin: ["@babel/plugin-transform-runtime"],
+            plugins: ["@babel/plugin-transform-runtime"],
           },
         },
       },
